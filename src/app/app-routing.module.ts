5763a451b75609ec14ee3d63adb29b9984c80079
Gabotw <u202018627@upc.edu.pe>
2024-09-05 23:04:26 -0500

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './public/pages/history/history.component';
+import { ConfigComponent } from './public/pages/config/config.component';
+import {NotificationsComponent} from './public/pages/notifications/notifications.component';
+import {SearchRoutesComponent} from './public/pages/search-routes/search-routes.component';
+import {PromosComponent} from './public/pages/promos/promos.component';
+import {HelpComponent} from './public/pages/help/help.component';
+import {RegisterComponent} from './public/pages/register/passenger/register.component';
+import {LoginPassengerComponent} from './public/pages/login/passenger/login-passenger.component';
+import {SideBarComponent} from "./public/components/side-bar/side-bar.component";
+import {RecoverPasswordComponent} from "./public/pages/recover-password/recover-password.component";
+import {NotFoundComponent} from "./public/components/not-found/not-found.component";
+import {LoginTypeSelectionComponent} from "./public/pages/login/type-selection/login-type-selection.component";
+import {RegisterDriverComponent} from "./public/pages/register/driver/register-driver.component";
+import {LoginDriverComponent} from "./public/pages/login/driver/login-driver.component";
+
+export const routes: Routes = [
+    { path: 'register/passenger', component: RegisterComponent },
+    {path: 'register/driver', component: RegisterDriverComponent},
+    { path: 'login', component: LoginTypeSelectionComponent},
+    {path: 'login/passenger', component: LoginPassengerComponent},
+    {path: 'login/driver', component: LoginDriverComponent},
+    { path: 'recover-password', component: RecoverPasswordComponent},
+    { path: 'sidebar', component: SideBarComponent, children: [
+            { path: 'notifications', component: NotificationsComponent },
+            { path: 'search', component: SearchRoutesComponent },
+            { path: 'promos', component: PromosComponent },
+            { path: 'history', component: HistoryComponent },
+            { path: 'help', component: HelpComponent },
+            { path: 'config', component: ConfigComponent },
+        ]},
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    {path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,77 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {ConfigComponent} from "./public/pages/config/config.component";
+import {HelpComponent} from "./public/pages/help/help.component";
+import {HistoryComponent} from "./public/pages/history/history.component";
+import {LoginPassengerComponent} from "./public/pages/login/passenger/login-passenger.component";
+import {NotificationsComponent} from "./public/pages/notifications/notifications.component";
+import {PayTicketComponent} from "./public/pages/pay-ticket/pay-ticket.component";
+import {PromosComponent} from "./public/pages/promos/promos.component";
+import {RegisterComponent} from "./public/pages/register/passenger/register.component";
+import {SearchRoutesComponent} from "./public/pages/search-routes/search-routes.component";
+import {SideBarComponent} from "./public/components/side-bar/side-bar.component";
+import { RecoverPasswordComponent } from './public/pages/recover-password/recover-password.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput} from "@angular/material/input";
+import { LayoutModule } from '@angular/cdk/layout';
+import { NotFoundComponent } from './public/components/not-found/not-found.component';
+import { LoginTypeSelectionComponent } from './public/pages/login/type-selection/login-type-selection.component';
+import { RegisterDriverComponent } from './public/pages/register/driver/register-driver.component';
+import { LoginDriverComponent } from './public/pages/login/driver/login-driver.component'
+import {MatCheckbox} from "@angular/material/checkbox";
+import { HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecoverPasswordComponent,
+    NotFoundComponent,
+    LoginTypeSelectionComponent,
+    RegisterDriverComponent,
+    LoginDriverComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ConfigComponent,
+        HelpComponent,
+        HistoryComponent,
+        LoginPassengerComponent,
+        NotificationsComponent,
+        PayTicketComponent,
+        PromosComponent,
+        RegisterComponent,
+        SearchRoutesComponent,
+        SideBarComponent,
+        FormsModule,
+        MatButton,
+        MatCard,
+        MatCardHeader,
+        MatCardSubtitle,
+        MatCardTitle,
+        MatFormField,
+        MatIcon,
+        MatInput,
+        MatLabel,
+        ReactiveFormsModule,
+        LayoutModule,
+        MatCardContent,
+        MatCheckbox,
+        MatIconButton,
+        MatSuffix,
+        HttpClientModule
+    ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
