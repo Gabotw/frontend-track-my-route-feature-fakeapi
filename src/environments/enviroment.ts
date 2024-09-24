3c752beaa97ca4b6a213021731140437ce625368
Gabotw <u202018627@upc.edu.pe>
2024-09-24 14:41:49 -0500

    feat: update backend link.

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     //serverBasePath: 'https://40.71.173.200/api/v1'
     //serverBasePath: 'https://9842-2800-200-e670-1e5-cc94-371c-a026-8108.ngrok-free.app/api/v1'
-    serverBasePath: 'https://trackmyroute-a0bzf2apedcva6g7.canadacentral-01.azurewebsites.net/api/v1'
+    serverBasePath: 'https://tmrapp-a2fke7f7hwfpcudh.brazilsouth-01.azurewebsites.net/api/v1'
 };
